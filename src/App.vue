192af66e41c69576ae8676c82ec5c9f42bae5ae7
EGOFOXLAB <egofoxlab@gmail.com>
2019-05-31 02:29:34 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="app">
+		<fieldset>
+			<legend>Audio form</legend>
+			<div>
+				<input type="range" min="-100" max="100" id="volume">
+			</div>
+		</fieldset>
+		<canvas id="visualizer" height="200"></canvas>
+	</div>
+</template>
+
+<script lang="ts">
+	import {Component, Vue} from 'vue-property-decorator';
+	import HelloWorld from './components/HelloWorld.vue';
+
+	@Component({
+		components: {
+			HelloWorld,
+
+		},
+	})
+	export default class App extends Vue {
+	}
+</script>
+
+<style lang="scss">
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
